23ee8ef0018b29f65e6f19d27ef20b2f7ee3bf16
Lu0969 <44342841+Lu0969@users.noreply.github.com>
2019-05-18 17:46:01 +0600

    Add files via upload

---
+++ src/com/company/RandomNumber.java
@@ -0,0 +1,24 @@
+package com.company;
+import java.util.Random;
+
+public class RandomNumber {
+    public int[] random() {                        //рандомирование числа для игры
+        int i, j;
+        Random rand = new Random();
+        int[] array = new int[4];
+
+        for (i = 0; i < array.length; ) {
+            int randomNumber = rand.nextInt(9);
+            for (j = 0; j < i; j++) {
+                if (array[j] == randomNumber) {
+                    break;
+                }
+            }
+            if (j == i) {
+                array[i] = randomNumber;
+                i++;
+            }
+        }
+        return array;
+    }
+}
